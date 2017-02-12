9c49f07c741b721e5acb210f4cf562407bfb0540
jimontoyag <icha9127@gmail.com>
2017-02-11 23:01:40 -0500

    Primera versión final (V1.0) :balloon:
    Flujo de comando para el cubo en el servicio.
    
    * Se podrían encapsular las instrucciones como servicios rest
    particulares. Ejm /cubo/update, /cubo/ini, /cubo/query :smirk: .  Pero
    siguiendo el ejercicio :rage3: se deja la implementación del flujo para
    esta versión.

--- src/main/java/com/bytespree/backrest/service/CuboService.java
+++ src/main/java/com/bytespree/backrest/service/CuboService.java
@@ -9,14 +9,47 @@ public class CuboService {
 	
 	private Cubo3D instanciaCubo;
 	
-	public String cuboProces(String comando) throws NumberFormatException{
+	private Integer operaciones;
+	
+	private Integer M;
+	
+	public String cuboProces(String comando) throws NumberFormatException, IndexOutOfBoundsException{
 		String respuesta;
-		if(this.instanciaCubo == null){
-			this.instanciaCubo = new Cubo3D(Integer.valueOf(comando).intValue());
-			respuesta=comando.toString();
-		}else{
-			respuesta="Vieo cubo";
-		}
+		if(operaciones == null){
+			this.operaciones = Integer.valueOf(comando);
+			respuesta = this.operaciones.toString();
+		}else if(this.instanciaCubo == null)
+			{
+				String[] NM = comando.split("\\s+");
+				this.instanciaCubo = new Cubo3D(Integer.valueOf(NM[0]).intValue());
+				this.M = Integer.valueOf(NM[1]);
+				respuesta=comando.toString();
+			}else if(M > 0){
+				String[] sentencia = comando.split("\\s+");
+				if(sentencia.length == 5){
+					this.instanciaCubo.update(Integer.valueOf(sentencia[1]), Integer.valueOf(sentencia[2]), 
+							Integer.valueOf(sentencia[3]), Integer.valueOf(sentencia[4]));
+					respuesta="";
+				}else if(sentencia.length == 7){
+					respuesta = this.instanciaCubo.query(Integer.valueOf(sentencia[1]), Integer.valueOf(sentencia[2]), 
+							Integer.valueOf(sentencia[3]), Integer.valueOf(sentencia[4]), Integer.valueOf(sentencia[5]), 
+							Integer.valueOf(sentencia[6]))+"";			
+				}else{
+					throw new NumberFormatException("comando:"+comando);
+				}
+				M--;
+			}else{
+				this.operaciones--;
+				if(this.operaciones == 0){
+					this.operaciones = null;
+					this.instanciaCubo = null;
+					respuesta = this.cuboProces(comando);
+				}else{
+					this.instanciaCubo = null;
+					respuesta = this.cuboProces(comando);
+				}
+			}
+		
 		return respuesta;
 	}
 	
